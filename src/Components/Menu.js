052d7800fda525e011178ef315da17f707c29d7f
jmokate <jmokate@gmail.com>
2020-09-30 17:30:36 -0500

    working on splash page

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,28 +2,16 @@ import React, { useState, useRef } from "react";
 import { Navbar, Nav, Overlay, Tooltip } from "react-bootstrap";
 
 function Menu() {
-	const [isHover, setHover] = useState(false);
-	const target = useRef(null);
-
 	return (
 		<Navbar variant='light' expand='md'>
-			<Navbar.Brand href='#home'>Muh Bagz</Navbar.Brand>
+			<Navbar.Brand href='#home'>📈📉</Navbar.Brand>
 			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 			<Navbar.Collapse id='responsive-navbar-nav'>
 				<Nav>
-					<Nav.Link
-						ref={target}
-						onMouseOver={() => setHover(true)}
-						onMouseLeave={() => setHover(false)}
-						href='#home'
-					>
-						🏠
-					</Nav.Link>
-					<Overlay target={target.curent} show={isHover} placement='top'>
-						{props => <Tooltip class='home-tip'>Home</Tooltip>}
-					</Overlay>
-					<Nav.Link href='#link'>💰</Nav.Link>
-					<Nav.Link href='#signin'>Sign In</Nav.Link>
+					<Nav.Link href='#home'>Home🏠</Nav.Link>
+
+					<Nav.Link href='#link'>Muh Bagz💰</Nav.Link>
+					<Nav.Link href='#signin'>Sign In🔑</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
--- src/Components/SplashPage.js
+++ src/Components/SplashPage.js
@@ -1,11 +1,57 @@
 import React, { useState } from "react";
 import Menu from "./Menu";
-import { Nav, Navbar } from "react-bootstrap";
+import "../App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function SplashPage() {
 	return (
 		<>
 			<Menu />
+			<Container fluid>
+				<Row noGutters>
+					<Col class='mx-auto'>
+						<h1 class='display-4' style={{ textAlign: "center" }}>
+							Welcome To
+						</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<h1 class='display-1' style={{ textAlign: "center" }}>
+							🤑
+						</h1>
+					</Col>
+					<Col>
+						<h1 class='display-1 text-nowrap' style={{ textAlign: "center" }}>
+							Muh Bagz!
+						</h1>
+					</Col>
+					<Col>
+						<h1 class='display-1' style={{ textAlign: "center" }}>
+							😭
+						</h1>
+					</Col>
+				</Row>
+				<Row></Row>
+				<Row>
+					<Col>
+						<h1 class='display-4 text-nowrap' style={{ textAlign: "center" }}>
+							Be A Chad!
+						</h1>
+					</Col>
+					<Col>
+						<img
+							className='splashImg'
+							src='https://media.giphy.com/media/tQ4Ppppmjy3jVS7H2Y/giphy.gif'
+						/>
+					</Col>
+					<Col>
+						<h1 class='display-4 text-nowrap' style={{ textAlign: "center" }}>
+							Don't Get Rekt!
+						</h1>
+					</Col>
+				</Row>
+			</Container>
 		</>
 	);
 }
