ebad151da556cc9d9929edaf4b14e403daf7b7d8
jmokate <jmokate@gmail.com>
2020-10-07 15:59:35 -0500

    positioning content

--- src/Components/SplashPage.js
+++ src/Components/SplashPage.js
@@ -16,49 +16,62 @@ function SplashPage() {
 					</Col>
 				</Row>
 				<Row>
-					<Col>
+					<Col
+						align='left'
+						xs={{ span: 12, order: 2 }}
+						sm={{ span: 12, order: 2 }}
+						md={{ span: 12, order: 2 }}
+						xl={{ span: 4, order: 1 }}
+					>
 						<h1 class='display-1' style={{ textAlign: "center" }}>
 							🤑
 						</h1>
-					</Col>
-					<Col>
-						<h1 class='display-1 text-nowrap' style={{ textAlign: "center" }}>
-							Muh Bagz!
+						<h1
+							align='center'
+							class='display-4 text-nowrap'
+							style={{ textAlign: "center" }}
+						>
+							Make Gainz!
 						</h1>
 					</Col>
-					<Col>
+					<Col
+						align='center'
+						xs={{ span: 12, order: 1 }}
+						sm={{ span: 12, order: 1 }}
+						md={{ span: 12, order: 1 }}
+						xl={{ span: 4, order: 2 }}
+					>
+						<h1 class='display-1 text-nowrap' style={{ textAlign: "center" }}>
+							Muh-Bagz!
+						</h1>
+						<Image
+							class='d-block mx-auto img-fluid'
+							className='splashImg'
+							src='https://media.giphy.com/media/tQ4Ppppmjy3jVS7H2Y/giphy.gif'
+						/>
+					</Col>
+					<Col
+						align='right'
+						style={{ paddingLeft: "4px" }}
+						xs={{ span: 12, order: 3 }}
+						sm={{ span: 12, order: 3 }}
+						md={{ span: 12, order: 3 }}
+						xl={{ span: 4, order: 3 }}
+					>
 						<h1 class='display-1' style={{ textAlign: "center" }}>
 							😭
+						</h1>
+						<h1 class='display-4 text-nowrap' style={{ textAlign: "center" }}>
+							Get Rekt!
 						</h1>
 					</Col>
 				</Row>
 				<Row></Row>
 				<Row>
-					<Col>
-						<h1 class='display-4 text-nowrap' style={{ textAlign: "center" }}>
-							Make Gainz!
-						</h1>
-						<Image 
-							class='justify-content-md-left'
-							src="https://media.giphy.com/media/ADgfsbHcS62Jy/giphy.gif"
-							/>
-					</Col>
-					<Col>
-						<Image
-							class='justify-content-md-center'
-							className='splashImg'
-							src='https://media.giphy.com/media/tQ4Ppppmjy3jVS7H2Y/giphy.gif'
-						/>
-					</Col>
-					<Col>
-						<h1 class='display-4 text-nowrap' style={{ textAlign: "center" }}>
-							Don't Get Rekt!
-						</h1>
-					</Col>
-					
-					</Row>
-				
-				
+					<Col></Col>
+					<Col align='center'></Col>
+					<Col></Col>
+				</Row>
 			</Container>
 		</>
 	);
